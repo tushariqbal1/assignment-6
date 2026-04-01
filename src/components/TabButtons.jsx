323f60ef1d4201367893b97a2c10022a4259150f
tushariqbal1 <tushariqbal331@gmail.com>
2026-04-01 12:41:00 +0600

    design tab section

--- src/components/TabButtons.jsx
+++ src/components/TabButtons.jsx
@@ -1,27 +1,36 @@
 function TabButtons({ activeTab, setActiveTab, totalItems }) {
   return (
-    <div className="flex justify-center gap-4 mt-8 mb-12">
-      <button
-        onClick={() => setActiveTab('products')}
-        className={`px-6 py-2 rounded-full font-medium transition-all ${
-          activeTab === 'products'
-            ? 'bg-purple-600 text-white shadow'
-            : 'bg-gray-100 text-gray-700 hover:bg-gray-200'
-        }`}
-      >
-        Products
-      </button>
 
-      <button
-        onClick={() => setActiveTab('cart')}
-        className={`px-6 py-2 rounded-full font-medium transition-all ${
-          activeTab === 'cart'
+    <div >
+      <div className="text-center mb-12">
+        <h2 className="text-4xl font-bold text-[#101727] mb-3">
+          Premium Digital Tools
+        </h2>
+        <p className=" text-[#627382] max-w-2xl mx-auto text-lg">
+          Choose from our curated collection of premium digital products designed to boost your productivity and creativity.
+        </p>
+      </div>
+      <div className="flex justify-center gap-4 mt-8 mb-12">
+        <button
+          onClick={() => setActiveTab('products')}
+          className={`px-6 py-2 rounded-full cursor-pointer font-medium transition-all ${activeTab === 'products'
             ? 'bg-purple-600 text-white shadow'
             : 'bg-gray-100 text-gray-700 hover:bg-gray-200'
-        }`}
-      >
-        Cart ({totalItems})
-      </button>
+            }`}
+        >
+          Products
+        </button>
+
+        <button
+          onClick={() => setActiveTab('cart')}
+          className={`px-6 py-2 rounded-full cursor-pointer font-medium transition-all ${activeTab === 'cart'
+            ? 'bg-purple-600 text-white shadow'
+            : 'bg-gray-100 text-gray-700 hover:bg-gray-200'
+            }`}
+        >
+          Cart ({totalItems})
+        </button>
+      </div>
     </div>
   );
 }
